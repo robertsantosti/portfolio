b75989a62b09994f414f4aa90851382258bad9fb
Robert Santos <robertsantosti@outlook.com>
2023-07-07 09:53:54 -0300

    Montando estrutura HTML do Header

---
+++ src/components/Header.component.tsx
@@ -0,0 +1,12 @@
+
+export const HeaderComponent = () => {
+  return(
+    <header>
+      <div className="logo-wrapper">
+        <p>Robert</p>
+        <p className="detaild">Santos</p>
+        <p>Ti</p>
+      </div>
+    </header>
+  ); 
+}
