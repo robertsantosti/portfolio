0248b8e7c96819811a611ee1ca7a6e2ea7ee3522
Robert Santos <robertsantosti@outlook.com>
2023-07-07 18:02:51 -0300

    Correção: remoção do MouseEvent não utilizado

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
   test: {
     globals: true,
     environment: 'jsdom',
