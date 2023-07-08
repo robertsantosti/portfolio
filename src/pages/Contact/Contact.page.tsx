2aa955ba5683e3cc65e2535ce5d0d01eebc4e24d
Robert Santos <robertsantosti@outlook.com>
2023-07-07 23:31:05 -0300

    Inst. Lib 'File Saver' + Funcionalidade de Download do CV

--- src/pages/Contact/Contact.page.tsx
+++ src/pages/Contact/Contact.page.tsx
@@ -1,13 +1,18 @@
+import { saveAs } from 'file-saver';
+import { MdCloudDownload } from 'react-icons/md';
+
+import CvFile from '../../assets/pdf/curriculo.pdf';
 import { ButtonComponent } from '../../components/Button/Button.component';
 import { SocialLinksComponent } from '../../components/SocialLinks/SocialLinks.component';
 import { SOCIAL_LINKS } from '../../utils/const/social-links';
 import { IPageProps } from '../../utils/interfaces/pages';
-
-import { MdCloudDownload } from 'react-icons/md'
 import * as Styled from './Contact.style';
 
-
 export const ContactPage = ({ errorPage }: IPageProps) => {
+  const saveFile = () => {
+    saveAs(CvFile, 'Curriculo - Robert Santos.pdf');
+  }
+
   const render = () => {
     return(
       <Styled.ContactPageWrapper>
@@ -20,7 +25,7 @@ export const ContactPage = ({ errorPage }: IPageProps) => {
           <SocialLinksComponent links={SOCIAL_LINKS} size='2rem'/>
 
         
-          <ButtonComponent>
+          <ButtonComponent onClick={saveFile}>
             <MdCloudDownload className="icon"/>
             <span>Download CV</span>
           </ButtonComponent>
